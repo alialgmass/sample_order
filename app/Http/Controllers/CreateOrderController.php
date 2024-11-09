f858809c139080a4db49403104ce7fba03c04805
eali7 <eali7563@gmail.com>
2024-11-10 00:36:15 +0200

    frist commit

--- app/Http/Controllers/CreateOrderController.php
+++ app/Http/Controllers/CreateOrderController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateOrderRequest;
-use App\Jobs\CreateOrderJob;
+use App\Services\OrderService;
 use Illuminate\Http\JsonResponse;
 
 class CreateOrderController extends BaseController
 {
-    public function __invoke(CreateOrderRequest $request): JsonResponse
+    public function __invoke(CreateOrderRequest $request, OrderService $orderService): JsonResponse
     {
         $validated = $request->validated();
-        CreateOrderJob::dispatchSync($validated);
+        $orderService->createOrder($validated);
 
         return $this->getResponse(200);
     }
